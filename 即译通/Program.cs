a7f7a9699304608e1510b14a962d95ad1ee59adb
白 李 <2857211723@qq.com>
2024-09-03 11:26:09 +0800

    添加项目文件。

---
+++ 即译通/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 即译通
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
